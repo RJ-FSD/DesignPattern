83d5183a8fb010d4d594abb56ca7d3c23edb5fb9
Kentaroh Nagai <kentaroh_nagai@ricoh-japan.co.jp>
2017-10-11 13:54:21 +0900

    AddSingletonPattern

---
+++ SingletonSample/SingletonSample/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SingletonSample
+{
+    public class Singleton
+    {
+        private static Singleton _singleton = new Singleton();
+
+        private Singleton()
+        {
+            Console.WriteLine("インスタンスを生成しました。");
+        }
+
+        public static Singleton GetInstance()
+        {
+            return _singleton;
+        }
+    }
+}
