19ceb457025735a7e05079e5d5c9e67f646ebd1e
Yoshii Aya <aya_yoshii@ricoh-japan.co.jp>
2017-10-11 15:44:26 +0900

    add FactoryPattern

---
+++ FactoryPatternConsole/Framework/Factory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    public abstract class Factory
+    {
+        public Product Create(string owner)
+        {
+            Product p = CreateProduct(owner);
+            RegisterProduct(p);
+            return p;
+        }
+
+        protected abstract Product CreateProduct(string owner);
+        protected abstract void RegisterProduct(Product product);
+    }
+}
