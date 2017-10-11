3cb5900a5cc9a7e63665a9ddd7750391a27a81df
nanakusajp <k.divocmkva.147@gmail.com>
2017-10-11 13:51:58 +0900

    Adapterパターンのサンプルコード追加

---
+++ AdapterPattern-Delegation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern_Delegation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrint print = new PrintBanner("Hello");
+            print.PrintWeak();
+            print.PrintStrong();
+
+            Console.ReadLine();
+        }
+    }
+}
