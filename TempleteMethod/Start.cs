90fcd86d322d30731f76c740274834dae17d0b8a
yuka kimura <yuka_a_kimura@ricoh-japan.co.jp>
2017-10-11 13:52:19 +0900

    templatemethodのサンプルを追加

---
+++ TempleteMethod/Start.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TempleteMethod
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            AbstractDisplay charDisplay = new CharDisplay('H');
+            AbstractDisplay stringDisplayOnce = new StringDisplay("こんにちは");
+            AbstractDisplay stringDisplayTwice = new StringDisplay("こんばんわ");
+
+            charDisplay.display();
+            stringDisplayOnce.display();
+            stringDisplayTwice.display();
+
+            Console.ReadLine();
+        }
+    }
+}
